b3af425d2fab55ceec4c361fc737c4bdfdb9cacc
Alla Matveyeva <alla.matveyeva.job@gmail.com>
2022-01-11 16:05:21 +0300

    add useWindowSize to Menu

--- my-app/src/components/menu/menu.jsx
+++ my-app/src/components/menu/menu.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import { withTranslator } from "../../hoc/withTranslator";
@@ -6,23 +6,29 @@ import MenuSmall from "../menuSmall/menuSmall";
 import "./menu.scss";
 
 function Menu(props) {
-  const screenWidth = window.screen.width;
-  // const [size, setSize] = useState("big");
+  const [size, setSize] = useState(0);
   const links = [
     { textId: props.translate("menu.button.characters"), id: "/alphabet" },
     { textId: props.translate("menu.button.books"), id: "/books" },
     { textId: props.translate("menu.button.movie"), id: "/movies" },
   ];
 
-  // useEffect(() => {
-  //   if (screenWidth <= 950) {
-  //     setSize("small");
-  //   }
-  // }, [screenWidth]);
+  function useWindowSize() {
+    useLayoutEffect(() => {
+      function updateSize() {
+        setSize(window.screen.width);
+      }
+      window.addEventListener("resize", updateSize);
+      updateSize();
+      return () => window.removeEventListener("resize", updateSize);
+    }, []);
+    return size;
+  }
+  useWindowSize();
 
   return (
     <>
-      {screenWidth >= 950 ? (
+      {size > 960 ? (
         <div className="menu__buttons">
           {links.map((link, index) => (
             <button key={index} className="menu__button">
